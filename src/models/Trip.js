c2a8ab14d287815bcbb841f1278e48b25cc695b3
Fatme <fatme34saeed@gmail.com>
2025-04-24 13:39:25 +0300

    Updates on Trip model, its routes and controller after adding locations, Users model

--- src/models/Trip.js
+++ src/models/Trip.js
@@ -1,34 +1,39 @@
-// models/Trip.js
 import mongoose from 'mongoose';
 
 const tripSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-  },
-  destination: {
-    type: String,
-    required: true,
+    trim: true
   },
   description: {
     type: String,
+    required: true,
+    trim: true
   },
+  locations: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Location',
+    required: true
+  }],
   startDate: {
     type: Date,
-    required: true,
+    required: true
   },
   endDate: {
     type: Date,
+    required: true
   },
   price: {
     type: Number,
-    required: true,
+    required: true
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
 
 const Trips = mongoose.model('Trips', tripSchema);
 export default Trips;
