734f8e005c16cbf0d52de4deb1ebbb207a4143cd
Fatme <fatme34saeed@gmail.com>
2025-04-25 10:39:17 +0300

    Removed verifyToken from getting all trips and trip by id

--- src/routes/tripRoutes.js
+++ src/routes/tripRoutes.js
@@ -11,8 +11,8 @@ import { verifyToken, checkRole } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // Routes for managing trips
-router.get('/', verifyToken, getTrips);  // Get all trips
-router.get('/:tripId', verifyToken, getTripById);  // Get trip by ID
+router.get('/', getTrips);  // Get all trips
+router.get('/:tripId', getTripById);  // Get trip by ID
 router.post('/', verifyToken, checkRole(['admin', 'super_admin']), createTrip);  // Create a new trip
 router.put('/:tripId', verifyToken, checkRole(['admin', 'super_admin']), updateTrip);  // Update trip by ID
 router.delete('/:tripId', verifyToken, checkRole(['admin', 'super_admin']), deleteTrip);  // Delete trip by ID
