49c2ee220d601759ed68535e974a5ffd600f38f2
Fatme <fatme34saeed@gmail.com>
2025-05-08 18:41:59 +0300

    Fix CORS and cookie handling for auth

--- src/server.js
+++ src/server.js
@@ -16,7 +16,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
